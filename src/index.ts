63eb695217120924b344e6e5590664fc47d2f192
Tabassum-Wasila <twahsila@gmail.com>
2026-01-07 22:15:33 +0800

    Create basic in memory database

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+interface Pokemon {
+    id: string;
+    attack: number;
+    defense: number;
+}
+
+interface BaseRecord{
+    id: string;
+}
+
+interface Database <T extends BaseRecord>{
+    set(newValue: T): void
+    get(id: string): T | undefined
+}
+
+class InMemoryDatabase<T extends BaseRecord> implements Database<T> {
+    private db: Record<string, T> = {};
+
+    public set(newValue: T): void {
+        this.db[newValue.id] = newValue;
+    }
+
+    public get(id: string): T | undefined{
+        return this.db[id];
+    }
+}
+
+const pokemonDB = new InMemoryDatabase<Pokemon>();
+
+pokemonDB.set({ id: 'Bulbasaur', attack: 30, defense: 15 });
+
+console.log(pokemonDB.get('Bulbasaur'));
